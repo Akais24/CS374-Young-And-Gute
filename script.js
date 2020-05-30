f98ee721708ee199461a475773f5fd15db3094a8
Akais24 <akqq24@gmail.com>
2020-05-30 21:26:05 +0900

    Implement fail page

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ function enter_intro() {
     $("#section").html('<object type="text/html" data="intro.html" style="width:100%;height:100%"></object>');
 }
 
-enter_intro();
+function enter_fail() {
+    $("#section").html('<object type="text/html" data="fail.html" style="width:100%;height:100%"></object>');
+}
