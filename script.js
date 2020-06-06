7a5c8b8ee6d851e41b1b645c1f6aa41b23932e68
Akais24 <akqq24@gmail.com>
2020-06-06 19:49:28 +0900

    Move intro/fail html/css to directory

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function enter_intro() {
-    $("#section").html('<object type="text/html" data="intro.html" style="width:100%;height:100%"></object>');
+    $("#section").html('<object type="text/html" data="intro/intro.html" style="width:100%;height:100%"></object>');
 }
 
 function enter_fail() {
-    $("#section").html('<object type="text/html" data="fail.html" style="width:100%;height:100%"></object>');
+    $("#section").html('<object type="text/html" data="fail/fail.html" style="width:100%;height:100%"></object>');
 }
