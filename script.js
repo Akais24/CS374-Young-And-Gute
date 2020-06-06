0618f4b58d8db1fd9766cdc56e7041cfb39468c8
김지석 <wltjr1002@gmail.com>
2020-06-06 19:55:31 +0900

    Move result html/css to directory
    make result folder

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function enter_result() {
-    $("#section").append('<object type="text/html" data="result.html" style="width:100%;height:100%;"></object>');
+    $("#section").append('<object type="text/html" data="result/result.html" style="width:100%;height:100%;"></object>');
     slide();
 }
 
