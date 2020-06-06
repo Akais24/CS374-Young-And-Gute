8fa31ad9190b13aa7106361ea599e9186b408206
Yeongbin Kim <ybin77@kaist.ac.kr>
2020-06-06 23:17:42 +0900

    conflict check

--- queries.js
+++
@@ -1,4 +0,0 @@
-var queries = [
-    { "question": "Q. What is the question This question is long, it should be two-line question?",
-        "choices": ["choice1", "choice2", "choice3", "choide4", "choice5"]}
-]
